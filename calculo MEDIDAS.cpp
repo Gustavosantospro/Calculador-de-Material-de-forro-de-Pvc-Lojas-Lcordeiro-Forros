685730df45f7bce47b8f1e87d7263879d0fcbfee
Gustavo Vieira <60290436+Gustavosantospro@users.noreply.github.com>
2021-03-09 09:27:42 -0300

    Update calculo MEDIDAS.cpp

--- calculo MEDIDAS.cpp
+++ calculo MEDIDAS.cpp
@@ -6,11 +6,12 @@
 		float pvc, perfilF, tubo, parafuso, buchas;
 		
 		// CALCULO DE PVC
-		/* Os forros de pvc são vendidos em placas/peças que variam de 6 à 8 metros de comprimento, variando a cada 1 metro. 
-		Por conta disso, podem ser vendidas de acordo com a necessidade dos cômodos dos clientes: se as medidas são
-		multiplas de 2 metros, podem ser utilizadas peças com 6, 8, para se ter melhor aproveitamento; se a medida
-		de instalção for 3,50, opta-se por vender peças com 7m, tendo em vista o menor desperdício de material e assim 
-		por diante.*/
+		/* Os forros de pvc sÃ£o vendidos em placas/peÃ§as que variam de 6 Ã  8 metros de comprimento, variando a cada 1 metro. 
+		Por conta disso, podem ser vendidas de acordo com a necessidade dos cÃ´modos dos clientes: se as medidas sÃ£o
+		multiplas de 2 metros, podem ser utilizadas peÃ§as com 6, 8, para se ter melhor aproveitamento; se a medida
+		de instalÃ§Ã£o for 3,50, opta-se por vender peÃ§as com 7m, tendo em vista o menor desperdÃ­cio de material e assim 
+		por diante.
+		Entenda-se como principal a medida de sentido de instalaÃ§Ã£o das placas de pvc; e secundÃ¡ria a largura.*/
 			
 		if(principal > 2.51 && principal <= 3.00 ){
 			pvc = ((secundaria/0.2)/2);
@@ -41,18 +42,19 @@
 		
 		// CALCULO DE PERFIL F
 		perfilF = (((principal*2)+(secundaria*2))/6);
-		/* O perfil é um acessório da instalação do forro que é necessário nas 4 paredes do cômodo. É então feita
-		a soma linear dos cômodos e dividido por 6m, pois cada perfil se vende a peça com 6 metros de comprimento*/
+		/* O perfil Ã© um acessÃ³rio da instalaÃ§Ã£o do forro que Ã© necessÃ¡rio nas 4 paredes do cÃ´modo. Ã‰ entÃ£o feita
+		a soma linear dos cÃ´modos e dividido por 6m, pois cada perfil se vende a peÃ§a com 6 metros de comprimento*/
 		
 		// CALCULO DE TUBO
 		tubo = (((principal/0.7)*secundaria)/6); // arredondamento
-		/* Os tubos são acessórios que servem para fixação do forro de pvc: o pvc é fixado ao tubo por meio de 
-		parafusos e os tubos são fixados no telhado, ou na lage, por meio de linhas de arame galvanizado. Os Tubos
-		são fixados no sentindo transversal do sentindo de instalção do forro e a cada 70cm de distância*/		
+		/* Os tubos sÃ£o acessÃ³rios que servem para fixaÃ§Ã£o do forro de pvc: o pvc Ã© fixado ao tubo por meio de 
+		parafusos e os tubos sÃ£o fixados no telhado, ou na lage, por meio de linhas de arame galvanizado. Os Tubos
+		sÃ£o fixados no sentindo transversal do sentindo de instalÃ§Ã£o do forro e a cada 70cm de distÃ¢ncia*/	
+	 
 		// CALCULO DE PARAFUSOS
 		parafuso = ((((principal*2)+(secundaria*2))/0.3)+((secundaria/0.2)*(principal/0.7)));
-		/* Para cada 30 cm de metro linear, no perfil, põe-se um parafuso e uma bucha; parafusos também são utilizados
-		na fixação do forro: nesse caso, a medida secundária dividida p/ 0.2(pois cada placa de pvc têm 20cm)
+		/* Para cada 30 cm de metro linear, no perfil, pÃµe-se um parafuso e uma bucha; parafusos tambÃ©m sÃ£o utilizados
+		na fixaÃ§Ã£o do forro: nesse caso, a medida secundÃ¡ria dividida p/ 0.2(pois cada placa de pvc tÃªm 20cm)
 		multilicado pela principal dividida por 0.7*/
 		
 		// CALCULO DE BUCHA
